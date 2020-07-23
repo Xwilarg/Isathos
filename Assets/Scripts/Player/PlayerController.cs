938acdff2f3df425e8c2c87b4190f6a3589b6723
Xwilarg <xwilarg@yahoo.fr>
2020-07-24 01:08:33 +0200

    Add player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterSprites _playerSprites;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+
+    private Direction _dir;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _dir = Direction.UP;
+    }
+
+    private void FixedUpdate()
+    {
+        // Movements
+        _rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * 10f;
+
+        // Set direction depending of velocity
+        if (Mathf.Abs(_rb.velocity.x) > Mathf.Abs(_rb.velocity.y))
+        {
+            if (_rb.velocity.x > 0)
+                _dir = Direction.RIGHT;
+            else if (_rb.velocity.x < 0)
+                _dir = Direction.LEFT;
+        }
+        else
+        {
+            if (_rb.velocity.y > 0)
+                _dir = Direction.UP;
+            else if (_rb.velocity.y < 0)
+                _dir = Direction.DOWN;
+        }
+
+        // Set sprite direction
+        switch (_dir)
+        {
+            case Direction.UP:
+                _sr.sprite = _playerSprites.up;
+                break;
+
+            case Direction.DOWN:
+                _sr.sprite = _playerSprites.down;
+                break;
+
+            case Direction.LEFT:
+                _sr.sprite = _playerSprites.left;
+                break;
+
+            case Direction.RIGHT:
+                _sr.sprite = _playerSprites.right;
+                break;
+        }
+    }
+
+    private enum Direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/CharacterSprites.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ScriptableObject/CharacterSprites", fileName = "CharacterSprites")]
+public class CharacterSprites : ScriptableObject
+{
+    public Sprite up, down, left, right;
+}
