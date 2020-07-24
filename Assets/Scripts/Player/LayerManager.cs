c200c2f34e23c15fb24e898d539506eeab7497d0
Xwilarg <xwilarg@yahoo.fr>
2020-07-24 02:06:39 +0200

    Fix layer issues

---
+++ Assets/Scripts/Player/LayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManager : MonoBehaviour
+{
+    private List<SpriteRenderer> _sprites;
+    private Transform _playerTransform;
+
+    private void Start()
+    {
+        _sprites = new List<SpriteRenderer>();
+        _playerTransform = transform.parent;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SpriteRenderer _sr = collision.GetComponent<SpriteRenderer>();
+        if (_sr != null)
+            _sprites.Add(_sr);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        SpriteRenderer _sr = collision.GetComponent<SpriteRenderer>();
+        if (_sr != null)
+            _sprites.Remove(_sr);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var sr in _sprites)
+        {
+            sr.sortingOrder = sr.transform.position.y < _playerTransform.position.y ? 1 : -1;
+        }
+    }
+}
