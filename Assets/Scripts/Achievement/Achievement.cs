fc9b7bcfc8e5b1a3b03693af7cb580543ef0b714
Xwilarg <xwilarg@yahoo.fr>
2020-09-01 14:52:02 +0200

    Add achievement dictionary

---
+++ Assets/Scripts/Achievement/Achievement.cs
@@ -0,0 +1,16 @@
+﻿namespace Achievement
+{
+    public class Achievement
+    {
+        public Achievement(string name, string description, int progression)
+        {
+            _name = name;
+            _description = description;
+            _maxProg = progression;
+        }
+
+        private string _name;
+        private string _description;
+        private int _maxProg;
+    }
+}
--- Assets/Scripts/Achievement/AchievementManager.cs
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Achievement
 {
     public class AchievementManager : MonoBehaviour
     {
         public static AchievementManager S { private set; get; }
+
+        private Dictionary<AchievementID, Achievement> _achievements;
 
         [SerializeField]
         private GameObject _gameObject;
@@ -17,6 +20,8 @@ namespace Achievement
         public void DisplayAchievement(AchievementID id)
         {
             var go = Instantiate(_gameObject, transform);
+
+            _achievements = new Dictionary<AchievementID, Achievement>();
         }
     }
 }
